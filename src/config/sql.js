87982e06af8e74b68192b95a9737459cc4158bc7
yealetssee <gegilabadze@gmail.com>
2023-09-03 21:14:17 +0400

    initial commit with connection with database already made

---
+++ src/config/sql.js
@@ -0,0 +1,17 @@
+import pgk from "pg";
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-cjqb5c8jbais739i2trg-a",
+  port: 5432,
+  database: "todos_38ui",
+  user: "todos_38ui_user",
+  password: "KMW0AevL8KwDI0n6kbigsVUKeRSDvKWQ",
+});
+
+export const createTable = async () => {
+  return await pool.query(
+    "CREATE TABLE IF NOT EXISTS todos (id SERIAL PRIMARY KEY, value TEXT, completed BOOLEAN)",
+  );
+};
