a4bc91740255e64f38923f38ed243df4e610fdef
yealetssee <gegilabadze@gmail.com>
2023-09-03 21:41:20 +0400

    first api call

--- src/server.js
+++ src/server.js
@@ -1,3 +1,22 @@
 import express from "express";
+import { createTable } from "./config/sql.js";
 
 const app = express();
+
+const init = async () => {
+  try {
+    await createTable();
+    startServer();
+  } catch (error) {
+    console.log(error);
+  }
+
+  function startServer() {
+    app.get("/", (_, response) => {
+      return response.status(200).json({ message: "working" });
+    });
+    app.listen(3000);
+  }
+};
+
+init();
